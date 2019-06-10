e49f0d7507aca1caba1fdd5466b7bf7c951adc4a
Style1002 <1002821768@qq.com>
2019-06-11 00:13:43 +0800

    feat(js对象):js对象的创建使用
    java8 StreamAPI
    stramAPI简单练习
    
    java8 StreamAPI
    stramAPI简单练习

---
+++ src/main/java/com/gouge/template/java8/streamAPI/StreamTest.java
@@ -0,0 +1,11 @@
+package com.gouge.template.java8.streamAPI;
+
+/**
+ * @author GouGe
+ * @data 2019/5/29 17:25
+ */
+public class StreamTest {
+    /**
+     * Stream：自己不会储存元素、不改变源数据、操作延迟(带需要时执行)
+     */
+}
